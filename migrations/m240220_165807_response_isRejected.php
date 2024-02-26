04de9e7094d8a0b3816dccf884fc3053b07ff805
Seraf-seraf <serafim123sima@mail.ru>
2024-02-26 13:40:57 +0600

    Добавление возможности радобавления откликов и принятия отклика заказчиком

---
+++ migrations/m240220_165807_response_isRejected.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m240220_165807_response_isRejected
+ */
+class m240220_165807_response_isRejected extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m240220_165807_response_isRejected cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m240220_165807_response_isRejected cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
